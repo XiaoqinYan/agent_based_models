2eb3030a10a4eeda2a29f8adf913c6046569f24b
Xiaoqin Yan <yanxiaoq@msu.edu>
2020-10-01 15:41:13 -0400

    Update cell.py

--- cell.py
+++ cell.py
@@ -15,3 +15,20 @@ DEAD = 0
         self.x, self.y = pos
         self.state = init_state
         self._nextState = None
+@property
+    def isAlive(self):
+        return self.state == self.ALIVE
+
+    @property
+    def neighbors(self):
+        return self.model.grid.neighbor_iter((self.x, self.y), True)
+
+    
+    def step(self):
+        """
+        Compute if the cell will be dead or alive at the next tick.  This is
+        based on the number of alive or dead neighbors.  The state is not
+        changed here, but is just computed and stored in self._nextState,
+        because our current state may still be necessary for our neighbors
+        to calculate their next state.
+        """
