d274ab010eeabe95f597a98bf6b36d645de7810a
Xiaoqin Yan <yanxiaoq@msu.edu>
2020-10-01 15:41:57 -0400

    Update cell.py

--- cell.py
+++ cell.py
@@ -32,3 +32,15 @@ DEAD = 0
         because our current state may still be necessary for our neighbors
         to calculate their next state.
         """
+        # Get the neighbors and apply the rules on whether to be alive or dead
+        # at the next tick.
+        live_neighbors = sum(neighbor.isAlive for neighbor in self.neighbors)
+
+        # Assume nextState is unchanged, unless changed below.
+        self._nextState = self.state
+        if self.isAlive:
+            if live_neighbors < 2 or live_neighbors > 3:
+                self._nextState = self.DEAD
+        else:
+            if live_neighbors == 3:
+                self._nextState = self.ALIVE        
