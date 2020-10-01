540f5b89db03674216bf7dff15076defb5b10c3c
Xiaoqin Yan <yanxiaoq@msu.edu>
2020-10-01 15:40:34 -0400

    Update cell.py

--- cell.py
+++ cell.py
@@ -4,3 +4,14 @@ from mesa import Agent
 class Cell(Agent):
     """Represents a single ALIVE or DEAD cell in the simulation."""
     
+DEAD = 0
+    ALIVE = 1
+
+    def __init__(self, pos, model, init_state=DEAD):
+        """
+        Create a cell, in the given state, at the given x, y position.
+        """
+        super().__init__(pos, model)
+        self.x, self.y = pos
+        self.state = init_state
+        self._nextState = None
